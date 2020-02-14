b2a4d31e6d2122857effdcb14e3b9df9d6ee3278
Landon Turner <lturner@LTurner-MacBook-Pro.local>
2020-02-14 11:21:48 -0500

    cards structured

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,30 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+function Article () {
+    const articleCard = document.createElement('div'),
+          headLine = document.createElement('div'),
+          authorContainer = document.createElement('div'),
+          imgContainer = document.createElement('div'),
+          authImg = document.createElement('img'),
+          authorName = document.createElement('span');
+    
+    articleCard.appendChild(headLine);
+    articleCard.appendChild(authorContainer);
+    authorContainer.appendChild(imgContainer);
+    authorContainer.appendChild(authorName);
+    imgContainer.appendChild(authImg);
+    
+    articleCard.classList.add('card');
+    headLine.classList.add('headline');
+    authorContainer.classList.add('author');
+    imgContainer.classList.add('img-container');
+    
+    return articleCard;
+}
+// console.log(Article())
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+  .then (response => {
+    console.log(response)
+})
